473f83d9ba9871afbefa086fdcc1b396b4ad220f
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-07-31 16:20:54 +0700

    Update Commit

--- igettime.php
+++ igettime.php
@@ -4,7 +4,7 @@ $json = json_decode($input);
 
 if (is_file("data/server_time.json")) {
     $content = file_get_contents("data/server_time.json");
-    $data = json_decode($content);
+    $data = json_decode($content, true);
 } else {
     $data['server_time'] = gmdate('Y-m-d H:i:s');
     $data['update_time'] = $data['server_time'];
