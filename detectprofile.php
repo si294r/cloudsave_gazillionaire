9a1d48ef4b2ee7e18573638a2a76d7059e08f816
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-11-06 18:28:21 +0700

    Update Commit

--- detectprofile.php
+++ detectprofile.php
@@ -14,22 +14,22 @@ if ($facebook_id == "") {
     return array("error" => 1, "message" => "facebook_id is required");
 }
 
-//$picture = file_get_contents("https://graph.facebook.com/v2.10/$facebook_id/picture");
+$picture = file_get_contents("https://graph.facebook.com/v2.10/$facebook_id/picture?width=1000");
 
-//$s3ClientS3 = new S3Client(array(
-//    'credentials' => array(
-//        'key' => $aws_access_key_id,
-//        'secret' => $aws_secret_access_key
-//    ),
-//    "region" => "us-east-1",
-//    "version" => "2006-03-01"
-//));
-//
-//$resultS3 = $s3ClientS3->putObject(array(
-//    'Bucket' => "alegrium-www",
-//    'Key'    => "gazillionaire/images/profile/{$facebook_id}.jpg",
-//    'Body'   => $picture
-//));
+$s3ClientS3 = new S3Client(array(
+    'credentials' => array(
+        'key' => $aws_access_key_id,
+        'secret' => $aws_secret_access_key
+    ),
+    "region" => "us-east-1",
+    "version" => "2006-03-01"
+));
+
+$resultS3 = $s3ClientS3->putObject(array(
+    'Bucket' => "alegrium-www",
+    'Key'    => "gazillionaire/images/profile/{$facebook_id}",
+    'Body'   => $picture
+));
 
 //var_dump($result);
 
