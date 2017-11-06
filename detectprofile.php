317880174dbd8c33fe4910f8bca817904458f312
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-11-06 17:31:29 +0700

    Update Commit

---
+++ detectprofile.php
@@ -0,0 +1,34 @@
+<?php
+
+require '/var/www/vendor/autoload.php';
+include '/var/www/redshift-config2.php';
+
+use Aws\S3\S3Client;
+use Aws\Rekognition\RekognitionClient;
+
+
+$json = json_decode($input);
+$facebook_id = isset($json->facebook_id) ? $json->facebook_id : "";
+
+if ($facebook_id == "") {
+    return array("error" => 1, "message" => "facebook_id is required");
+}
+
+$picture = file_get_contents("https://graph.facebook.com/v2.10/$facebook_id/picture");
+
+$clientS3 = new S3Client(array(
+    'credentials' => array(
+        'key' => $aws_access_key_id,
+        'secret' => $aws_secret_access_key
+    ),
+    "region" => "us-east-1",
+    "version" => "2006-03-01"
+));
+
+$result = $clientS3->$s3->putObject(array(
+    'Bucket' => "alegrium-www",
+    'Key'    => "gazillionaire/images/profile/$facebook_id",
+    'Body'   => $picture
+));
+
+var_dump($result);
